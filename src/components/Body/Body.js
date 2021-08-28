dc00527b8b4aaabd71203cb8b940217a72b35f89
Coert73 <cg.grobbelaar@gmail.com>
2021-08-28 15:38:03 +0200

    WorkDone

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,10 +3,11 @@ import {
 	Switch,
 	Route
 } from "react-router-dom";
-import { BOOK_APPOINTMENT, LANDING } from "constants/routes";
+import { BOOK_APPOINTMENT, Confirm, LANDING } from "constants/routes";
 import BookAppointment from "views/BookAppointment/BookAppointment";
 import Landing from "views/Landing/Landing";
 import "./Body.css";
+import Confirmation from "views/Confirmation";
 
 export default class Body extends Component {
 
@@ -19,6 +20,9 @@ export default class Body extends Component {
 					</Route>
 					<Route path={LANDING}>
 						<Landing />
+					</Route>
+					<Route path={Confirm}>
+						<Confirmation />
 					</Route>
 				</Switch>
 			</div>
--- src/views/BookAppointment/BookAppointment.js
+++ src/views/BookAppointment/BookAppointment.js
@@ -1,15 +1,53 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Button } from "@material-ui/core";
+import { Confirm } from "constants/routes";
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import "./BookAppointment.css";
 
-export default class BookAppointment extends Component {
+export class BookAppointment extends Component {
+	static propTypes = {
+		match: PropTypes.object.isRequired,
+		location: PropTypes.object.isRequired,
+		history: PropTypes.object.isRequired
+	};
+	handleBookAppointmentClicks() {
+		this.props.history.push(Confirm);
+	}
 	render() {
 		return (
 			<Box className="book-appointment" display="flex" flexDirection="column" flex="1" alignItems="center" justifyContent="center">
 				<Typography m={1} variant="h3" component="h3">
-					{"Book Appointment Page"}
-				</Typography>
+					<h3>Please fill in the following details of the form</h3>
+					<p>Name</p>
+					<input type="text" name="firstName"/>
+					<p>Surame</p>
+					<input type="text"  name="lastName"/>
+					<p>Cellphone Number</p>
+					<input type="text"  name="cellphone"/>
+					<p>Email Address</p>
+					<input type="email" pattern=".+@globex\.com" size="30" name="email" required></input>
+					<p>Type of animal</p>
+					<select name="petType" id="animals">
+					    <option value="rabbit">Rabbit</option>
+						<option value="cat">Cat</option>
+						<option value="dog">Dog</option>
+						<option value="bird">Bird</option></select>
+					<p>ID/Passport Number</p>
+					<input type="text"  name="id"/>
+					<p>Date and Time Start</p>
+					<input type="datetime-local"  name="appointmentStartDate"></input>
+					<p>Date and Time End</p>
+					<input type="datetime-local"  name="appointmentEndDate"></input>
+					<p>Pet Name</p>
+					<input type="text"  name="vistReason"/>
+					<p>Confirm your work appointment below.</p>
+					<Button className="top-button" variant="contained" size="large" color="primary" onClick={()=>this.handleBookAppointmentClicks()}>{"Confirm Appointment"}</Button>
+				</Typography>	  
 			</Box>
+			
 		);
 	}
 }
+
+export default withRouter(BookAppointment);
